35d883364274ebb967ded7087792230e7e3a1eb0
Chris Allick <chrisallick@gmail.com>
2012-05-04 00:45:32 -0400

    done

--- js/main.js
+++ js/main.js
@@ -1,54 +1,36 @@
-;(function($) {
-    $.fn.textfill = function(options) {
-        var fontSize = options.maxFontPixels;
-        var ourText = $('span:visible:first', this);
-        var maxHeight = $(this).height();
-        var maxWidth = $(this).width();
-        var textHeight;
-        var textWidth;
-        do {
-            ourText.css({'font-size': fontSize, 'line-height': fontSize+"px"});
-            textHeight = ourText.height();
-            textWidth = ourText.width();
-            fontSize = fontSize - 1;
-        } while ((textHeight > maxHeight || textWidth > maxWidth) && fontSize > 3);
-        return this;
-    }
-})(jQuery);
-
-/**
-*  http://chris-barr.com/entry/disable_text_selection_with_jquery/
-*  modified to be “$” safe by Dakkar Daemor [www.imaginific.com]
-*/
-;(function($) {
-	$.fn.disableTextSelect = function() {
-		return this.each(function() {
-			$(this).mousedown(function(){return false;});
-		});
-	}
-})(jQuery);
-
-/*
-	compatible with ecs-0.0.1.js
-*/
-
-// create ecs connection
-// connect and load callbacks
-// when message comes in from backend check to see if it's the playlist data
-// load them all into the playlist
-// set the scrolling
-// set the click events
-// on click using YTJS api to load video
-// when video is over fire off event!
-
 var ytplayer;
 var video_data = {};
 var loaded = false;
 var current_index = 0;
 
 $(document).ready( function() {
+	$("#play_pause").click(function(event) {
+		event.preventDefault();
+		if( $(this).html() == "pause" ) {
+			$(this).html("play");
+			ytplayer.pauseVideo();
+		} else {
+			$(this).html("pause");
+			ytplayer.playVideo();
+		}
+	});
+
+	$("#next").click(function(event){
+		event.preventDefault();
+		playNextVideo();
+	});
+
+	$(document).bind("idle.idleTimer", function(){
+		$("#videos").slideUp();
+		$("#controls").slideUp();
+	});
+
+	$(document).bind("active.idleTimer", function(){
+		$("#videos").slideDown();
+		$("#controls").slideDown();
+	});
+
 	loadVideos( shuffle( all_videos ) );
-	//console.log( all_videos );
 });
 
 loadVideos = function( videos ) {
@@ -59,6 +41,9 @@ loadVideos = function( videos ) {
 	initVideos( );
 }
 
+/*
+	this is dope!
+*/
 shuffle = function(o) {
 	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
 	return o;
@@ -71,7 +56,7 @@ initVideos = function() {
 			if( !$(this).hasClass("playing") ) {
 				switchVideo( $(this).attr("id") );
 			}
-		}).disableTextSelect();
+		});
 	});
 
 	playNextVideo();
@@ -86,6 +71,7 @@ playNextVideo = function() {
 		switchVideo( vid );
 		if( loaded == false ) {
 			loaded = true;
+			$.idleTimer(1500);
 		}
 	} else {
 		switchVideo( $(".playing").next().attr("id") );
@@ -93,8 +79,7 @@ playNextVideo = function() {
 }
 
 onYouTubePlayerReady = function( playerId ) {
-	console.log("hell?");
-	ytplayer = document.getElementById( "myytplayer" );
+	ytplayer = document.getElementById( "video_container" );
   	ytplayer.addEventListener("onStateChange", "onytplayerStateChange");
   	ytplayer.addEventListener("onError", "onytplayerError");
 
@@ -103,18 +88,22 @@ onYouTubePlayerReady = function( playerId ) {
 
 switchVideo = function( vid ) {
 	if( !$("#video_container").hasClass( vid ) ) {
+		// pop playing and set new playing
 		$(".playing").removeClass("playing");
 		$("#"+vid).addClass("playing");
-		var params = { allowScriptAccess: "always" };
-    	var atts = { id: "myytplayer" };
-    	if( $("#myytplayer").length ) {
-    		swfobject.embedSWF( "http://www.youtube.com/v/"+vid+"?enablejsapi=1&playerapiid=ytplayer&version=3", "myytplayer", "628", "353", "8", null, null, params, atts );
-    	} else {
-    		swfobject.embedSWF( "http://www.youtube.com/v/"+vid+"?enablejsapi=1&playerapiid=ytplayer&version=3", "video_container", "628", "353", "8", null, null, params, atts );
-    	}
+
+		// embed vid
+		var params = { allowScriptAccess: "always", wmode: "transparent" };
+    	var atts = { id: "video_container" };
+    	swfobject.embedSWF( "http://www.youtube.com/v/"+vid+"?enablejsapi=1&playerapiid=ytplayer&version=3&controls=0", "video_container", "100%", "100%", "9", null, null, params, atts );
 		
 		var playing_index = $(".playing").index();
 
+		// slide left index times width of first video
+		var dist = "-" + (playing_index * $('#videos .video:first').width()) + "px";
+		$("#videos").css("left",dist);
+
+		// update play index
 		current_index = playing_index;
 	}
 }
@@ -125,6 +114,7 @@ function onytplayerStateChange(newState) {
 	}
 }
 
+// on error just go to next vid
 function onytplayerError(error) {
 	playNextVideo()
 }
